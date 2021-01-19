2cc8821d555f1335c92924d1e2a24d75b29bdf1b
vatana chhorn <47716174+VatanaChhorn@users.noreply.github.com>
2021-01-19 14:50:25 +0700

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Vatana Chhorn on 19/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
